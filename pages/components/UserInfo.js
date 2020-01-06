8162b26a901efaa8f7e864bacd9f456afcd6c5c9
wuxianqiang <2631640352@qq.com>
2020-01-06 22:43:37 +0800

    模块的懒加载

--- pages/components/UserInfo.js
+++ pages/components/UserInfo.js
@@ -1,10 +1,19 @@
+import { useState } from "react"
+import {Button} from 'antd'
+
 function UserInfo (props) {
   // props.user
+  let [created, setCreated] = useState(props.user.createTime)
+  async function changeFormat () {
+    let moment = await import('moment')
+    setCreated(moment(created).format('YYYY-MM-DD'))
+  }
   return (
     <div>
       <p>用户名：{props.user.username}</p>
       <p>密码：{props.user.password}</p>
-      <p>创建时间：{props.user.createTime}</p>
+      <p>创建时间：{created}</p>
+      <Button onClick={changeFormat}>转换时间</Button>
     </div>
   )
 }
